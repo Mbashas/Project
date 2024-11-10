910aa18137682f413aa995de492e4ba2ce6d4ced
sethmbasha <sethmbasha@gmail.com>
2024-11-10 22:03:45 +0300

    Remove  metadata name from restaurant page

--- campus-food-management/restaurant.php
+++ campus-food-management/restaurant.php
@@ -13,7 +13,7 @@ $result = $stmt->get_result();
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="author" content="Sahil Kumar">
+  <meta name="author" content="">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Guild Canteen</title>
